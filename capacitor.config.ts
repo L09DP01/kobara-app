de88983ba4e9b8c78fd96f5f734d09f2bd4ee6f9
Marozo07 <509omni.vp@gmail.com>
2026-05-25 10:57:37 -0400

    Fix capacitor.config.ts type error for Vercel build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.kobara.app',
   appName: 'Kobara',
   webDir: 'public',
-  bundledWebRuntime: false,
   server: {
     url: 'https://kobara.app/mobile-onboarding',
     cleartext: true
