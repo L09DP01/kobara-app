d787d5cece8a4489d12d88848ac94956f20a3db2
Marozo07 <509omni.vp@gmail.com>
2026-05-24 21:32:45 -0400

    Fix Vercel build type error in manifest.ts

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -24,8 +24,6 @@ export default function manifest(): MetadataRoute.Manifest {
       {
         src: '/apple-touch-icon.png',
         sizes: '180x180',
-        type: 'image/png',
-        purpose: 'apple touch icon'
       }
     ],
   };
