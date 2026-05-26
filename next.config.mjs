4af2acac37962930583413fb5f875d5043d272c6
Marozo07 <509omni.vp@gmail.com>
2026-05-25 23:00:38 -0400

    Fix static assets on pay.kobara.app by using afterFiles rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: '/:path*',
-          has: [
-            {
-              type: 'host',
-              value: 'pay.kobara.app',
-            },
-          ],
-          destination: '/pay/:path*',
-        },
-      ],
-    };
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'pay.kobara.app',
+          },
+        ],
+        destination: '/pay/:path*',
+      },
+    ];
   },
 };
 
