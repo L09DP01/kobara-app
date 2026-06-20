beb70ad66b3a6f6db955cb7c972eaec316084807
Marozo07 <509omni.vp@gmail.com>
2026-06-20 12:40:50 -0400

    fix: use single asterisk comment block to bypass strict plugin check parsing

--- wordpress-plugin/kobara-wordpress-plugin/kobara-wordpress-plugin.php
+++ wordpress-plugin/kobara-wordpress-plugin/kobara-wordpress-plugin.php
@@ -1,18 +1,18 @@
 <?php
-/**
- * Plugin Name: Kobara Payments for WooCommerce
- * Plugin URI: https://kobara.app/docs/wordpress-plugin
- * Description: Accept MonCash payments in WooCommerce through Kobara.
- * Version: 1.0.0
- * Author: Kobara
- * Author URI: https://kobara.app
- * License: GPLv2 or later
- * License URI: https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain: kobara-payments-for-woocommerce
+/*
+ * Plugin Name:       Kobara Payments for WooCommerce
+ * Plugin URI:        https://kobara.app/docs/wordpress-plugin
+ * Description:       Accept MonCash payments in WooCommerce through Kobara.
+ * Version:           1.0.0
  * Requires at least: 6.0
- * Requires PHP: 7.4
+ * Requires PHP:      7.4
+ * Author:            Kobara
+ * Author URI:        https://kobara.app
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       kobara-payments-for-woocommerce
  * WC requires at least: 7.0
- * WC tested up to: 9.0
+ * WC tested up to:   9.0
  */
 
 if (!defined('ABSPATH')) {
